03d2892d6dcf0df512fe63ff46f78e828fbab243
snugs <snugsworth@gmail.com>
2026-01-26 01:44:38 -0800

    Increase MAX_BARS and adjust tempo limits

--- js/constants.js
+++ js/constants.js
@@ -4,17 +4,19 @@ export const APP_VERSION = "0.1.0"; // Added application version
 
 export const STEPS_PER_BAR = 16;
 export const defaultStepsPerBar = 16; // Default for new tracks
-export const MAX_BARS = 32; // Maximum number of bars a sequence can have
+export const MAX_BARS = 512; // Maximum number of bars a sequence can have
 
-export const MIN_TEMPO = 30; // Minimum tempo in BPM
-export const MAX_TEMPO = 300; // Maximum tempo in BPM
+export const MIN_TEMPO = 0; // Minimum tempo in BPM
+export const MAX_TEMPO = 999; // Maximum tempo in BPM
 
 // Note: Reversed for typical top-to-bottom piano roll display in a UI
 export const synthPitches = [
+    'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
     'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
     'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
     'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
-    'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5'
+    'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
+    'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6'
 ].reverse();
 
 export const soundLibraries = {
