59b5edb6abe046123ce865b0406fbdf1e4c85420
solarlotus <solarlotus@zo.computer>
2026-04-22 13:38:39 +0000

    Automated daily merge & bug fix (2026-04-22)

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 // js/constants.js - Shared constants for SnugOS
 
-export const APP_VERSION = "0.2.1"; // Bumped for undo capture additions and setter methods
+export const APP_VERSION = "0.2.2"; // Daily merge: typo fix for isReconstructinging
 
 export const STEPS_PER_BAR = 16;
 export const defaultStepsPerBar = 16; // Default for new tracks
